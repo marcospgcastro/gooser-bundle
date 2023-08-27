e7093b3f089db99d5e57a68c79f1d2e9c0486637
marcospgcastro <marcospgcastro@if.uff.br>
2023-08-27 19:06:33 -0300

    Código comentado - aplicação para uso acadêmico, desenvolvimento de novos códigos e correção de futuros bugs.

--- src/services/modules/onlineTarget.tsx
+++ src/services/modules/onlineTarget.tsx
@@ -1,18 +1,18 @@
-import * as URL from 'url'
-import * as DNS from 'dns'
+import * as URL from 'url'                                                      // Importação de módulo externo url
+import * as DNS from 'dns'                                                      // Importação de módulo externo dns
 
-export abstract class NewHost {
-  private readonly _HostName: string                                            
+export abstract class NewHost {                                                 // Criação de classe abstrata
+  private readonly _HostName: string                                            // Tipagem de variável; o método "readonly" indica somente leitura
 
-  constructor(_HostName: string){                                                   
-    this._HostName = _HostName 
+  constructor(_HostName: string){                                               // Método construtor                                                   
+    this._HostName = _HostName                                                  // A variável declarada recebe o valor de _HostName
   }
 
-  onlineHost = (): void => {
-    DNS.lookup( String(URL.parse( this._HostName, true ).host), (error) => {
-      if(error && error.code === 'ENOTFOUND') {
-         console.error( ' Alvo não localizado - Gooser finalizado!')
-         throw error;
+  onlineHost = (): void => {                                                    // Definição de função que verifica se um endereço fornecido está online
+    DNS.lookup( String(URL.parse( this._HostName, true ).host), (error) => {    // Verificação de endereço usando as funções url.parse() e dns.lookup() 
+      if(error && error.code === 'ENOTFOUND') {                                 // Condição para verificação da existência de erros ao buscar o endereço
+         console.error( ' Alvo não localizado - Gooser finalizado!')            // Log de saída para caminho inexistente
+         throw error;                                                           // Emissão de erro e finalização da execução do aplicativo.
       }
     })
   }
