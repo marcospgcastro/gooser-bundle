e92e3328afbb01ff2d69a2fdabbbb265d1a27c92
marcospgcastro <marcospgcastro@if.uff.br>
2023-08-29 00:06:48 -0300

    Comentários ao código, utilizados como guia para construções futuras.

--- src/services/modules/createTarget.tsx
+++ src/services/modules/createTarget.tsx
@@ -13,62 +13,65 @@ export abstract class NewTarget {
 
     this._Address = _Address                                                    // Variável de endereçamento do alvo
     this._TrgName = _TrgName                                                    // Variável de definição de nomeação para o alvo
-    this._ScrDual = _ScrDual                                                    // Variável do tipo tupla, usando dupla precisão para definir resolução de imagem capturada
     this._Latency = _Latency                                                    // Variável de de definição de tempo de latência entre as verificação
     this._Logical = _Logical                                                    // Variável lógica para ajuste e controle de fluxo de dados
+    this._ScrDual = [                                                           // Variável do tipo tupla, usada para definir resolução de imagem capturada
+      this._ScrDual[0],                                                         // => Largura em pixels
+      this._ScrDual[1]                                                          // => Altura em pixels
+    ]                                          
   }
 
   showLogical = (): boolean => {                                                // Método que exibe status de Full Page
-    return this._Logical
+    return this._Logical                                                        // Retorna o valor da variável lógica de ajuste
   } 
 
   showLatency = (): number => {                                                 // Método que exibe latência de verificação
-    return this._Latency
+    return this._Latency                                                        // Retorna o valor da variável de latência 
   }
 
   showScrDual = (): [number, number] => {                                       // Método que exibe resolução: largura x altura - Pixels.
-    return [this._ScrDual[0],this._ScrDual[1]]
+    return [this._ScrDual[0],this._ScrDual[1]]                                  // Retorna o valor da tupla
   }
 
   showTrgName = (): string => {                                                 // Método que exibe nome do alvo
-    return this._TrgName
+    return this._TrgName                                                        // Retorna o valor da variável de nomeação
   }
 
   showAddress = (): string => {                                                 // Método que exibe endereço do alvo
-    return this._Address
+    return this._Address                                                        // Retorna o valor da variável de endereço
   }
 
   changeAddress = (_Address: string): void => {                                 // Método que altera o endereço do alvo
-    this._Address = _Address
-    console.log(`Endereço alterado para: ${this._Address}`)
+    this._Address = _Address                                                    // Redundância na atribuição de valores
+    console.log(`Endereço alterado para: ${this._Address}`)                     // Altera o valor da variável de endereço
   }
 
   changeTrgName = (_TrgName: string): void => {                                 // Método que altera o nome do alvo
-    this._TrgName = _TrgName
-    console.log(`Nome do alvo alterado para: ${this._TrgName}`)
+    this._TrgName = _TrgName                                                    // Redundância na atribuição de valores
+    console.log(`Nome do alvo alterado para: ${this._TrgName}`)                 // Retorna nome de diretório alterado
   }
 
   changeScrDual = (_ScrDual: [number, number] ): void => {                      // Método que altera a resolução: largura x altura - Pixels.
-    this._ScrDual = [_ScrDual[0], _ScrDual[1]]
-    console.log(`Dimensão alterada para: ${this._ScrDual[0]}px
-                 - ${this._ScrDual[1]}px`)
+    this._ScrDual = [_ScrDual[0], _ScrDual[1]]                                  // Redundância na atribuição de valores
+    console.log(`Dimensão alterada para: ${this._ScrDual[0]}px`+                // Retorna valores atualizados de resolução
+                `- ${this._ScrDual[1]}px`)                                      // Sentença concatenada com sinal '+'
   }
 
   changeLatency = (_Latency: number): void => {                                 // Método que altera a latência de verificação
-    this._Latency = _Latency
-    console.log(`A latência de verificação foi alterada para:
-                 ${this._Latency} minutos`)
+    this._Latency = _Latency                                                    // Redundância na atribuição de valores
+    console.log(`A latência de verificação foi alterada para:`+                 // Mensagem de alteração de latência de verificação 
+                `${this._Latency} minutos`)                                     // Sentença concatenada com sinal '+'
   }
 
   changeLogical = (): void => {                                                 // Método que altera FullPage
-    if( this._Logical === true ) {
-      this._Logical = false
-      console.log(`O alvo ${this._TrgName} cancelou a opção FullPage!`)
+    if( this._Logical === true ) {                                              // Condição de teste para mudança de valor lógico da variável
+      this._Logical = false                                                     // Atribuição de valores direta
+      console.log(`O alvo ${this._TrgName} cancelou a opção FullPage!`)         // Mensagem de cancelamento 
     }
 
-    if( this._Logical === false ) {
-      this._Logical = true
-      console.log(`O alvo ${this._TrgName} selecionou  a opção FullPage!`) 
+    if( this._Logical === false ) {                                             // Condição de teste para mudança de valor lógico da variável
+      this._Logical = true                                                      // Atribuição de valores direta
+      console.log(`O alvo ${this._TrgName} selecionou  a opção FullPage!`)      // Mensagem de confirmação
     }
   }
 }
