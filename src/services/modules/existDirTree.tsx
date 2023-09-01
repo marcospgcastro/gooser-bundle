057a05e051ee1c0e4cd734007f4fd581e97ab0e1
marcospgcastro <marcospgcastro@if.uff.br>
2023-09-01 14:42:01 -0300

    Função erro apresenta falha, o método usado para tratamento de erros no JavaScript não é aplicado ao código tipado.

--- src/services/modules/existDirTree.tsx
+++ src/services/modules/existDirTree.tsx
@@ -10,53 +10,53 @@ export abstract class DirTree {                                                 
   private _SiteName: string
   private _PathName: string
 
-  constructor(_SavePath: string, _HostName: string, _TrgName: string,
+  constructor(_SavePath: string, _HostName: string,
               _SiteName: string, _PathName: string){                            // Método construtor  
 
     this._HostName = _HostName                       
     this._SavePath = _SavePath                                                  // A variável declarada recebe o valor de _HostName
  
-    this._SiteName = String(URL.parse(_HostName, true).host);                            // default name - Seleciona nome do Host por link fornecido
-    this._PathName = '/'+RUN.basename(_HostName,                                 // default path
-                         RUN.extname(_HostName));
+    this._SiteName = String(URL.parse(this._HostName, true).host);                            // default name - Seleciona nome do Host por link fornecido
+    this._PathName = '/'+RUN.basename(this._HostName,                                 // default path
+                         RUN.extname(this._HostName));
  
   }
 
-  existPath = (_SavePath: string, _HostName: string, _TrgName: string,
+  existPath = (_SavePath: string, _HostName: string,                            // Verifica se o local onde os dados serão armazenados "existe" - não modifica.
                _SiteName: string, _PathName: string): void => {  
 
-  if (!SYS.existsSync(_SavePath+_SiteName+_PathName)) { 
+  if (!SYS.existsSync(this._SavePath+this._SiteName+this._PathName)) { 
     console.log(' Criando arvore de diretórios...')
 
-    if (!SYS.existsSync(_SavePath)) {                                         // Verifica caminho de "imagem" de forma Síncrona                                       
-        console.log(' Não foi localizado o caminho: '+_SavePath,
+    if (!SYS.existsSync(this._SavePath)) {                                         // Verifica caminho de "imagem" de forma Síncrona                                       
+        console.log(' Não foi localizado o caminho: '+this._SavePath,
         ' verifique seu diretório de imagens!')
-        // console.error(' Erro: '+_SavePath+' não localizado | '+err)                                                     // Emissão de erro
+        console.error(' Erro: '+this._SavePath+' não localizado | '+err)                                                     // Emissão de erro
         PRO.exit(1)
     }
-    if (!SYS.existsSync(_SavePath+_SiteName)) {                                // Verifica caminho de "imagem" de forma Síncrona                                       
-      SYS.mkdirSync(_SavePath+_SiteName, function(err){                        // Cria diretório em caso de ausência
-          console.log(' Falha na criação de: '+_SavePath+_SiteName)
-          console.error(' Erro: criação de /'+_SiteName+': '+err)                                                     // Emissão de erro
+    if (!SYS.existsSync(this._SavePath+this._SiteName)) {                                // Verifica caminho de "imagem" de forma Síncrona                                       
+      SYS.mkdirSync(this._SavePath+this._SiteName, function(err){                        // Cria diretório em caso de ausência
+          console.log(' Falha na criação de: '+this._SavePath+this._SiteName)
+          console.error(' Erro: criação de /'+this._SiteName+': '+err)                                                     // Emissão de erro
           PRO.exit(1)
        })
-       console.log(' Criado: '+_SavePath+_SiteName)
+       console.log(' Criado: '+this._SavePath+this._SiteName)
     }
-    if (!SYS.existsSync(_SavePath+_SiteName+_PathName)) {                       // Verifica caminho de "imagem" de forma Síncrona                                       
-      SYS.mkdirSync(_SavePath+_SiteName+_PathName, function(err){               // Cria diretório em caso de ausência
-          console.log(' Falha na criação de: '+_SavePath+_SiteName+_PathName)
-          console.error(' Erro: criação de /'+_PathName+': '+err)                                                     // Emissão de erro
+    if (!SYS.existsSync(this._SavePath+this._SiteName+this._PathName)) {                       // Verifica caminho de "imagem" de forma Síncrona                                       
+      SYS.mkdirSync(this._SavePath+this._SiteName+this._PathName, function(err){               // Cria diretório em caso de ausência
+          console.log(' Falha na criação de: '+this._SavePath+this._SiteName+this._PathName)
+          console.error(' Erro: criação de /'+this._PathName+': '+err)                                                     // Emissão de erro
           PRO.exit(1)
       })
-      console.log(' Criado: '+_SavePath+_SiteName+_PathName);
+      console.log(' Criado: '+this._SavePath+this._SiteName+this._PathName);
     }
     console.log(' Ainda não há arquivos no diretório.')
     return false;
   } else {
-    console.log(' Localizada arvore de diretórios: '+_SavePath+_SiteName+_PathName)
+    console.log(' Localizada arvore de diretórios: '+this._SavePath+this._SiteName+this._PathName)
 
-    var cont = SYS.readdirSync(_SavePath+_SiteName+_PathName, function(err){
-        console.log(' Falha na leitura de: '+_SavePath+_SiteName+_PathName)
+    var cont = SYS.readdirSync(this._SavePath+this._SiteName+this._PathName, function(err){
+        console.log(' Falha na leitura de: '+this._SavePath+this._SiteName+this._PathName)
         console.error(err)
         PRO.exit(1)                                                                 // Emissão de erro
     })
